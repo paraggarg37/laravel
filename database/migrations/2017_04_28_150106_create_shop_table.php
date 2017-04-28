39cf7bc7591c8c492819fa0d7d6fce8cd7cd226b
Parag Garg <paraggarg37@gmail.com>
2017-04-28 22:29:28 +0530

    added shop

---
+++ database/migrations/2017_04_28_150106_create_shop_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateShopTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shop', function (Blueprint $table) {
+
+            $table->increments('shop_id');
+            $table->string('shop_name');
+            $table->string('shop_description');
+            $table->string('shop_logo');
+            $table->string('shop_tnc');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('shop');
+    }
+}
---
+++ database/migrations/2017_04_28_152330_create_shop_product_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateShopProductTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shop_product', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('shop_id');
+            $table->integer('product_id');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('shop_product');
+    }
+}
