40855ca442d7a3e0c34a298fe913f83ddb33d77e
Parag Garg <paraggarg37@gmail.com>
2017-04-28 23:05:20 +0530

    fixed base url

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', '166.62.28.104'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'paragshop'),
+            'username' => env('DB_USERNAME', 'purrx'),
+            'password' => env('DB_PASSWORD', 'schacker'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
