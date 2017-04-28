ecb614efc01bfc24e9b0c016cd3436a3935301c8
Parag Garg <paraggarg37@gmail.com>
2017-04-28 22:34:25 +0530

    fixed base url

--- config/app.php
+++ config/app.php
@@ -51,7 +51,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost/shop/shop'),
+    'url' => env('APP_URL', 'https://rocky-beach-72303.herokuapp.com'),
 
     /*
     |--------------------------------------------------------------------------
