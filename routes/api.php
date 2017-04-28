8f7130056bb1b2598731a0ab5658186f6888cfe4
Parag Garg <paraggarg37@gmail.com>
2017-04-28 22:45:26 +0530

    fixed base url

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-//Route::resource('shop', 'ShopController');
-
-Route::get('shop/{id}', 'ShopController@show');
+Route::resource('shop', 'ShopController');
